5e8679d6387840d03fefc571baf913a87e7ead76
vandit333 <106046614+vandit333@users.noreply.github.com>
2022-06-21 16:05:05 +0530

    Create data.dart

---
+++ lib/data.dart
@@ -0,0 +1,44 @@
+
+
+
+
+import 'package:flutter/material.dart';
+
+class data
+{
+
+
+  static List<String> imagelist=["img/img1.jpg","img/img2.jpg","img/img3.jpg","img/img4.jpg","img/img2.jpg","img/img3.jpg","img/img4.jpg"];
+  static List<String> catlist=["love","friend","life","sad","funny","bewafa","attitude"];
+  static List<String> lovesh=["Hum Aapki Har Cheez Se Pyar Kar Lenge",
+                              "Nahi Jo Dil Mein Jagah To Najar Mein Rehne Do,",
+                              "Tere Khayal Se Khud Ko Chhupa Ke Dekha Hai",];
+
+  static List<String> friendsh=["Hum Aapki Har Cheez Se Pyar Kar Lenge ",
+                                "Nahi Jo Dil Mein Jagah To Najar Mein Rehne Do,",
+                                "Tere Khayal Se Khud Ko Chhupa Ke Dekha Hai",];
+  static List<String> lifesh=["hhhh Aapki Har Cheez Se Pyar Kar Lenge",
+                               "bbbbb Jo Dil Mein Jagah To Najar Mein Rehne Do,",
+                                "Tere Khayal Se Khud Ko Chhupa Ke Dekha Hai",];
+  static List<String> sadsh=["Hum Aapki Har Cheez Se Pyar Kar Lenge",
+                            "Nahi Jo Dil Mein Jagah To Najar Mein Rehne Do,",
+                              "Tere Khayal Se Khud Ko Chhupa Ke Dekha Hai",];
+  static List<String> funnysh=["Hum Aapki Har Cheez Se Pyar Kar Lenge",
+                                " Nahi Jo Dil Mein Jagah To Najar Mein Rehne Do,",
+                                "Tere Khayal Se Khud Ko Chhupa Ke Dekha Hai",];
+  static List<String> bewafash=["Hum Aapki Har Cheez Se Pyar Kar Lenge",
+                               "Nahi Jo Dil Mein Jagah To Najar Mein Rehne Do,",
+                                "Tere Khayal Se Khud Ko Chhupa Ke Dekha Hai",];
+  static List<String> attitudesh=["Hum Aapki Har Cheez Se Pyar Kar Lenge",
+                                "Nahi Jo Dil Mein Jagah To Najar Mein Rehne Do,",
+                                "Tere Khayal Se Khud Ko Chhupa Ke Dekha Hai",];
+  static List<String> emoji=["😊😂😘😁😎😉","👩‍🦰👨‍🦰👨‍🦱👩‍🦲👩‍🦲","🤤😒😓😔😕🙃"];
+
+  static List<String> font=["fml1","fml2","fml3","fml4","fml5","fml6","fml7","fml8","fml9","fml10"];
+
+  // static List colorlist = [Colors.blue,Colors.yellow,Colors.orangeAccent,Colors.deepOrange,Colors.blue,Colors.yellow,Colors.orangeAccent,Colors.deepOrange,Colors.blue,Colors.yellow,Colors.orangeAccent,Colors.deepOrange];
+
+  static List bcolors=[Color(0xff8EC5FC),Color(0xff80D0C7),Color(0xff8BC6EC),Color(0xffB5FFFC),Colors.black,Colors.blueGrey,Colors.green,Colors.black26,Color(0xffFAACA8),Color(0xffA9C9FF)];
+  static List colorlist=[Color(0xff8EC5FC),Color(0xff80D0C7),Color(0xff8BC6EC),Color(0xffB5FFFC),Colors.black,Colors.blueGrey,Colors.green,Colors.black26,Color(0xffFAACA8),Color(0xffA9C9FF)];
+  static String folderpath="";
+}
